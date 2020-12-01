313f66047a56daf93fb79d28a2f7c41b1efa653a
2-013 <1730186152@qq.com>
2020-12-01 11:43:22 +0800

    0.1首次

---
+++ src/java/com/bjpowernode/test1/T1.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.test1;
+
+/**
+ * @program: git_test
+ * @description
+ * @author: zyh
+ * @create: 2020-12-01 11:20
+ * @version:1.0.0
+ **/
+public class T1 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println("hello world");
+    }
+}
